579e419c9b8e14f69763ed2fe74c0b7c191d5371
anseon jang <64743348+anseonjang@users.noreply.github.com>
2024-01-18 23:50:45 +0900

    카드뉴스

---
+++ App.js
@@ -0,0 +1,21 @@
+import "./Group66.css";
+
+export const Group66 = ({ className, ...props }) => {
+  return (
+    <div className={"group-66 " + className}>
+      <div className="rectangle-6"></div>
+      <div className="rectangle-19"></div>
+      <div className="d-3">D - 3 </div>
+      <div className="group-18">
+        <div className="sk">
+          SK 바이오사이언스
+          <br />{" "}
+        </div>
+        <div className="_2024-01-12">~2024-01-12 </div>
+      </div>
+      <div className="_23-4-house-hr">
+        23년 4분기 HOUSE HR 경력사원 수시채용{" "}
+      </div>
+    </div>
+  );
+};
